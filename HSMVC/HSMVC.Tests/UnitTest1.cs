fac88903a8ab11424f9cad22bd0803db24688678
rc6886 <rc6886@gmail.com>
2016-01-03 20:58:51 -0600

    Adding initial repository.

---
+++ HSMVC/HSMVC.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HSMVC.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
